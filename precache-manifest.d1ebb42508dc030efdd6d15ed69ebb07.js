c29ea5596fe05f7bd0de768371526d501135d026
Arh3825 <alisonho38@gmail.com>
2025-04-08 00:36:12 -0500

    Updates

---
+++ precache-manifest.d1ebb42508dc030efdd6d15ed69ebb07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b6d31456d08919da86a8e4b201ad55",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "6873d6fee105dabe7d12",
+    "url": "/my-portfolio/static/css/main.25ff1a66.chunk.css"
+  },
+  {
+    "revision": "e6b756da09fad424e316",
+    "url": "/my-portfolio/static/js/2.6b544f0c.chunk.js"
+  },
+  {
+    "revision": "80c22b709854abab6800481175e1e427",
+    "url": "/my-portfolio/static/js/2.6b544f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6873d6fee105dabe7d12",
+    "url": "/my-portfolio/static/js/main.85f7c238.chunk.js"
+  },
+  {
+    "revision": "f8bc21605bac90413ed8",
+    "url": "/my-portfolio/static/js/runtime-main.cc7e5888.js"
+  },
+  {
+    "revision": "c9a2252176c31ffc2290184b55893b6e",
+    "url": "/my-portfolio/static/media/headshot.c9a22521.png"
+  }
+]);
